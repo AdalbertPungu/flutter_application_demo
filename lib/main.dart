f9fc1747fe7434c79342fdcbf45aff2d09062caa
Adalbert Pungu <18pw419@esisalama.org>
2022-04-17 00:41:17 +0200

    Ajout des boutons et la gestion des évènements

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,12 @@ class HomePage extends StatelessWidget {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Profil'),
+          title: Text('MyApp'),
         ),
-        body: Container(
-          color: Colors.blue.shade50,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(width: 70, height: 70, color: Colors.yellow),
-              ),
-              Expanded(
-                  child: Text(
-                      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptatum sapiente earum enim obcaecati! Facere animi suscipit harum tempora incidunt repudiandae quod tenetur placeat optio maiores porro, mollitia totam culpa!'))
-            ],
+        body: Center(
+          child: Column(
+            children: [],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
         ),
       ),
     );
