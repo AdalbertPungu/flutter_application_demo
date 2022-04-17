bf975b7752c6c3f16e68bbf8a9f50bd9972a4b74
Adalbert Pungu <18pw419@esisalama.org>
2022-04-17 04:06:13 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,17 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(OrangeValleyApp());
 }
 
-class HomePage extends StatelessWidget {
-  final euroList = [
-    'Allemagne',
-    'Autriche',
-    'Belgique',
-    'Bulgarie',
-    'Chypre',
-    'Croatie',
-    'Danemark',
-    'Espagne',
-    'Estonie',
-    'Finlande',
-    'France',
-    'Grèce',
-    'Hongrie',
-    'Irlande',
-    'Italie',
-    'Lettonie',
-    'Litunie',
-    'Luxembourg',
-    'Malte',
-    'Pays-Bas',
-    'Pologne',
-    'Portugal',
-    'Roumanie',
-    'Slovaque',
-    'Slovénie',
-    'Suède',
-    'Tchéquie',
-  ];
+class OrangeValleyApp extends StatelessWidget {
+  const OrangeValleyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyApp'),
-      ),
-      body: ListView.builder(
-        itemCount: euroList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(euroList[index]),
-            subtitle: Text(euroList[index]),
-            leading: CircleAvatar(
-              backgroundColor: Colors.blue,
-            ),
-          );
-        },
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
