dded422b40e097c6f75c560fe206cc0253f41f98
Adalbert Pungu <18pw419@esisalama.org>
2022-04-17 10:02:26 +0200

    disable debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class OrangeValleyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
